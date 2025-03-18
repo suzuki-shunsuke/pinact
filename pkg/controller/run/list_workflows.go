ed62a78045ee6c200f8e561ac292ef9017ac19af
Shunsuke Suzuki <suzuki-shunsuke@users.noreply.github.com>
2025-03-18 16:04:13 +0900

    feat: look for action.yaml and action.yml by default (#805)
    * feat: look for action.yaml and action.yml by default
    
    * ci: add a test

--- pkg/controller/run/list_workflows.go
+++ pkg/controller/run/list_workflows.go
@@ -3,16 +3,33 @@ package run
 import (
 	"fmt"
 	"path/filepath"
+
+	"github.com/sirupsen/logrus"
+	"github.com/suzuki-shunsuke/logrus-error/logerr"
 )
 
 func listWorkflows() ([]string, error) {
-	files, err := filepath.Glob(".github/workflows/*.yml")
-	if err != nil {
-		return nil, fmt.Errorf("find .github/workflows/*.yml: %w", err)
+	patterns := []string{
+		".github/workflows/*.yml",
+		".github/workflows/*.yaml",
+		"action.yml",
+		"action.yaml",
+		"*/action.yml",
+		"*/action.yaml",
+		"*/*/action.yml",
+		"*/*/action.yaml",
+		"*/*/*/action.yml",
+		"*/*/*/action.yaml",
 	}
-	files2, err := filepath.Glob(".github/workflows/*.yaml")
-	if err != nil {
-		return nil, fmt.Errorf("find .github/workflows/*.yaml: %w", err)
+	files := []string{}
+	for _, pattern := range patterns {
+		matches, err := filepath.Glob(pattern)
+		if err != nil {
+			return nil, fmt.Errorf("look for workflow or composite action files using glob: %w", logerr.WithFields(err, logrus.Fields{
+				"pattern": pattern,
+			}))
+		}
+		files = append(files, matches...)
 	}
-	return append(files, files2...), nil
+	return files, nil
 }
